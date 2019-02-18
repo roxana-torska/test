df9c43ba7fbbac58b4b0b09332727cd96d493f0a
Manbeena Brar <manbeena.brar@mind2minds.com>
2019-02-18 12:14:05 +0530

    server port change

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.prepare()
 			return handle(req, res)
 		})
 
-		server.listen(3000, (err) => {
+		server.listen(3001, (err) => {
 			if (err) throw err
-			console.log('> Ready on http://localhost:3000')
+			console.log('> Ready on http://localhost:3001')
 		})
 	})
 	.catch((ex) => {
