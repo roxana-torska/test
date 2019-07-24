5fbda7158ba193ea97a1a716909ae6cf45a4e03b
Niv Segal <nsegal@Global.Bio-Rad.com>
2019-07-24 17:24:04 +0300

    This is to remove IP module

--- utils/config.js
+++ utils/config.js
@@ -1,14 +1,11 @@
-var ip = require("ip");
-console.dir (  );
 const configs = function() {
   console.log('env', process.env.NODE_ENV);
-  console.log('ip', ip.address());
   switch (process.env.NODE_ENV) {
     case 'development':
       return {
-        APP_URL: 'http://localhost:3001',
-        API_URL: `${ip.address()}:8001`,
-        API_IMAGE_URL: 'http://localhost:8282'
+        APP_URL: 'http://ec2-35-156-241-115.eu-central-1.compute.amazonaws.com:3001',
+        API_URL: 'http://ec2-35-156-241-115.eu-central-1.compute.amazonaws.com:8282',
+        API_IMAGE_URL: 'http://ec2-35-156-241-115.eu-central-1.compute.amazonaws.com:8282'
       };
 
     case 'production':
