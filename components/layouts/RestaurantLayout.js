a7dc5f4b577de17b44dd97ee0b16e63ef004fe93
Maksym Shekhovtsev <29maxsh@gmail.com>
2020-09-25 06:06:08 -0400

    fix missed components

---
+++ components/layouts/RestaurantLayout.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Grid } from '@material-ui/core';
+import { observer } from 'mobx-react';
+import RestaurantHeader from '../header/RestaurantHeader';
+import AppFooter from '../footer/AppFooter';
+import { withStyles } from '@material-ui/core/styles';
+import Notifier from '../Notifier/Notifier';
+import styles from '../../styles/common';
+
+const splashScreen = {
+  width: '100vw',
+  height: '100vh',
+  display: 'flex',
+  fontSize: '0px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: '0px',
+  padding: '0px'
+};
+const splashLogo = {
+  width: '100px',
+  height: '100px',
+  backgroundImage: 'url(/static/imgs/logo.svg)',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundColor: '#f44336'
+};
+
+export default class RestaurantLayout extends Component {
+  state = {
+    hideSplash: false
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ hideSplash: true });
+    }, 500);
+  }
+  render() {
+    const {
+      children,
+      classes,
+      selectedPageTab,
+      toggleMenu,
+      changeOverlay,
+      menuData,
+      onMenuChange,
+      restaurantsName,
+      isDishDetails,
+      isHomePage,
+    } = this.props;
+    const { hideSplash } = this.state;
+    return hideSplash ? (
+      <React.Fragment>
+        <RestaurantHeader
+          selectedPageTab={selectedPageTab}
+          toggleMenu={toggleMenu}
+          changeOverlay={changeOverlay}
+          menuData={menuData}
+          isDishDetails={isDishDetails}
+          isHomePage={isHomePage}
+          // onMenuChange={onMenuChange}
+          restaurantsName={restaurantsName}
+        />
+        <div className={classes.headerMarginSuppress}>{children}</div>
+        <Notifier />
+        {/* <RestaurantFooter /> */}
+      </React.Fragment>
+    ) : (
+        <div style={splashScreen}>
+          <div style={splashLogo} />
+        </div>
+      );
+  }
+}
+
+// export default observer(withStyles(styles)(RestaurantLayout));
